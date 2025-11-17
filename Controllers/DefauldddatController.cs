a5b6d6e424a9a102690ffb55dfb2b9774c83be75
dunguyen2221 <dunguyen20112000@gmail.com>
2025-11-17 20:41:57 +0700

    ...

---
+++ Controllers/DefauldddatController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEBVANDAP.Controllers
+{
+    public class DefauldddatController : Controller
+    {
+        // GET: Defauldddat
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
