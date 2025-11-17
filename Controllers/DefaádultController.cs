e1cd07e4b936a23dfc6c3524dade9c6be75b51ed
dunguyen2221 <dunguyen20112000@gmail.com>
2025-11-17 20:40:30 +0700

    ...

---
+++ Controllers/DefaádultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEBVANDAP.Controllers
+{
+    public class DefaádultController : Controller
+    {
+        // GET: Defaádult
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
