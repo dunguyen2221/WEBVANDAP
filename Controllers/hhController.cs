9e8a96a4174668fc54d48e7eab91d3eb3d0dadcd
ntdat07 <datnguyen.300706@gmail.com>
2025-11-17 20:22:50 +0700

    Add files via upload

---
+++ Controllers/hhController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEBVANDAP.Controllers
+{
+    public class hhController : Controller
+    {
+        // GET: hh
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
