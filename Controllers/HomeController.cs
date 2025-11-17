b53338904bd39b66ee280c1ee17884830ccd389a
TysonPhan <phanminhtri16@gmail.com>
2025-11-17 18:41:47 +0700

    Update comment in HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Web.Mvc;
 using WEBVANDAP.Models;
 using System.Data.Entity;
 using System.Collections.Generic; // Cần thiết cho List
-//bautau
+//sybau
 namespace WEBVANDAP.Controllers
 {
     public class HomeController : Controller
