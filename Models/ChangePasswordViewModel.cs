76ceecf5b1d441f5c5148a48887b8e95c1749d11
TysonPhan <phanminhtri16@gmail.com>
2025-11-22 21:54:22 +0700

    update UI

--- Models/ChangePasswordViewModel.cs
+++ Models/ChangePasswordViewModel.cs
@@ -4,17 +4,18 @@ namespace WEBVANDAP.Models
 {
     public class ChangePasswordViewModel
     {
-        [Required]
+        [Required(ErrorMessage ="Vui lòng nhập mật khẩu hiện tại")]
         [DataType(DataType.Password)]
         [Display(Name = "Mật khẩu hiện tại")]
         public string OldPassword { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Nhập mật khẩu mới")]
         [StringLength(100, ErrorMessage = "Mật khẩu phải ít nhất {2} ký tự.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Mật khẩu mới")]
         public string NewPassword { get; set; }
 
+        [Required(ErrorMessage ="Vui lòng xác nhận mật khẩu mới")]
         [DataType(DataType.Password)]
         [Display(Name = "Xác nhận mật khẩu mới")]
         [Compare("NewPassword", ErrorMessage = "Xác nhận mật khẩu không khớp.")]
